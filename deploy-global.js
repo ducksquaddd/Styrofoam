d7c42de91f34351e8ea6eeadcd0220631cacc247
Verico <51441251+bqini@users.noreply.github.com>
2022-08-27 01:00:23 -0400

    Added 'clientID' to config.json, Modified balls.js, and am currently testing a way to deploy commands.

---
+++ deploy-global.js
@@ -0,0 +1,29 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v10');
+const { clientID, categories } = require('./config.json');
+
+const fs = require('node:fs');
+const path = require('node:path');
+
+let commands = [];
+let pro = new Promise((res, rej) => {
+    categories.forEach((ctg) => {
+        fs.readdir(path.resolve(`./commands/${ctg}/`), (err, files) => {
+            if(err) throw err;
+            let f = files.filter(x => x.endsWith('.js'));
+            f.forEach(file => {
+                commands.push(file);
+                res(commands);
+            })
+        })
+    })
+})
+
+
+pro.then((x) => {
+    const rest = new REST({ version: '10' }).setToken(process.env['TOKEN']);
+
+    rest.put(Routes.applicationCommands(clientID), { body: x })
+	.then(() => console.log('Successfully registered application commands globally.'))
+	.catch(console.error);
+});
