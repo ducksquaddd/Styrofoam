7dcba9fdab671a93dbd57f1a8a08729b3efe5341
Verico <51441251+bqini@users.noreply.github.com>
2022-08-27 05:29:37 -0400

    Unmute command.

---
+++ commands/moderation/unmute.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { PermissionsBitField } = require('discord.js');
+const Luxon = require('luxon');
+
+const data = new SlashCommandBuilder()
+    .setName("unmute")
+    .setDescription("Unmutes a person from your Discord server.")
+    .addMentionableOption(option =>
+        option.setName('target').setDescription('Select a user.').setRequired(true)
+    )
+    .addStringOption(option =>
+        option.setName('reason').setDescription('Reason as to why you want to mute this user.').setRequired(true)
+    )
+    .setDefaultMemberPermissions(PermissionsBitField.Flags.ModerateMembers);
+
+function execute(i) {
+    const member = i.options.getMentionable('target');
+    const reason = i.options.getString('reason');
+    try {
+        member.timeout(z, reason).then((x) => {
+            i.reply(`Unmuted ${x.user.username}. Reason: ${reason}`)
+        });
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+module.exports = { data, execute };
