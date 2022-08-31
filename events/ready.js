8619d03ccec5a7713fa9db9c8fbce38ad0ed47a5
Verico <51441251+bqini@users.noreply.github.com>
2022-08-31 02:33:49 -0400

    set bot to dnd, give it a custom status

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,22 @@
+const { ActivityType } = require("discord.js");
+
 module.exports = {
     event_type: "ready",
     run_once: true,
 
     event_run: async (bot) => {
-      console.log(`${bot.user.username} is ready.`)
+      console.log(`${bot.user.username} is ready.`);
+
+      bot.user.setPresence(
+        {
+            status: "dnd",
+            activities: [
+                {
+                    name: "Every new version of Discord.js is even more a pain in the ass than the last version.",
+                    type: ActivityType.Custom
+                }
+            ]
+        }
+      )
     }
   }
